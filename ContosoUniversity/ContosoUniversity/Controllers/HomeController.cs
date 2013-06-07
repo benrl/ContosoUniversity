b36aec7e9ace3bd2a6f137d6ae1fa59a0db16564
benrl <ben.linabury@makemoredigital.com>
2013-06-07 17:10:24 +0100

    Up to MODIFYING HOME CONTROLLER END OF PAGE 2

--- ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
